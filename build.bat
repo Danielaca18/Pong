4abaceb07b799227e1f16deea8377299c4032df1
Daniel Castro <dcastro@ualberta.ca>
2024-04-28 19:02:00 -0600

    removed unused cmake statements

--- build.bat
+++ build.bat
@@ -2,8 +2,15 @@
 rmdir /S/Q build
 mkdir build
 
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build
+set type=%1
+
+IF /i "%1"=="D" (
+    cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
+    cmake --build build
+) ELSE (
+    cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
+    cmake --build build --config Release
+)
 
 ::cmake --preset=default
 ::cmake --build build
